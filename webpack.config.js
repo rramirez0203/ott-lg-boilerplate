84e5cdd1e93882feb6d882811cfd62ce8508b705
rramirez0203 <roger.rahe@gmail.com>
2019-07-15 16:07:04 -0600

    css/sass setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 console.log("ON WEBPACK", __dirname);
 const path = require("path");
 const sourcePath = path.join(__dirname, "src");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 const config = {
   entry: path.resolve(sourcePath, "index.js"),
@@ -10,16 +11,52 @@ const config = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"]
+      },
+      {
+        // Apply rule for .sass, .scss or .css files
+        test: /\.(sa|sc|c)ss$/,
+
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+          {
+            // After all CSS loaders we use plugin to do his work.
+            // It gets all transformed CSS and extracts it into separate
+            // single bundled file
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            // This loader resolves url() and @imports inside CSS
+            loader: "css-loader"
+          },
+          {
+            // Then we apply postCSS fixes like autoprefixer and minifying
+            loader: "postcss-loader"
+          },
+          {
+            // First we transform SASS to standard CSS
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass")
+            }
+          }
+        ]
       }
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ["*", ".js", ".jsx"]
   },
   output: {
     path: __dirname + "/dist",
     publicPath: "/",
     filename: "bundle.js"
-  }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "bundle.css"
+    })
+  ]
 };
 module.exports = config;
